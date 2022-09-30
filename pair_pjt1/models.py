70619f7c5374f1d2090ef6b2ce2e5d80bfb26beb
caretim <karesin72041@gmail.com>
2022-09-30 10:36:03 +0900

    2

--- pair_pjt1/models.py
+++ pair_pjt1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Review(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.TextField(null= False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_ad = models.DateTimeField(auto_now=True)
